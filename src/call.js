f58d82265e4f904bde504657e2dc03a0ab1b323b
Danilo <danilosantrac96@gmail.com>
2022-04-23 02:41:47 +0200

    Fix check.js

--- src/call.js
+++ src/call.js
@@ -1,6 +1,6 @@
 const call = async (id) => {
     const response = await fetch(
-      `https://quiet-fortresponses-06303.herokuapp.com/api/chars/${id}`,
+      `www.app.com/api/chars/${id}`,
       { method: "post" }
     );
     const data = await response.json();
--- src/checkChar.js
+++ src/checkChar.js
@@ -1,49 +1,52 @@
 import { call } from "./call";
+const checkArr = [];
 
 const check = async (xCheck, yCheck) => {
-    const checkArr = [];
+    
     const charPosition = {
       waldo: {
-        id: 6,
+        id: 1,
         xmin: 669,
         xmax: 699,
         ymin: 363,
         ymax: 393
       },
       wanda: {
-        id: 7,
+        id: 2,
         xmin: 377,
         xmax: 407,
         ymin: 201,
         ymax: 231 
       },
       odlaw: {
-        id: 8,
+        id: 3,
         xmin: 240,
         xmax: 270,
         ymin: 313,
         ymax: 343
       },
       whitebeard: {
-        id: 9,
+        id: 4,
         xmin: 40,
         xmax: 70,
         ymin: 372,
         ymax: 402
       }
     };
-    for(let [key, value] of Object.entries(charPosition)){
-      if(value.xmin <= xCheck && value.xmax >= xCheck && value.ymin <= yCheck && value.ymax >= yCheck) {
-        if(checkArr.includes(value.id)) return;
-        console.log('hey');
-        let charName = await call(value.id);
-        console.log(`You found ${charName}!`);
-        checkArr.push(value.id);
-        return;
+    const chars = Object.keys(charPosition);
 
+    chars.forEach((key, index) => {
+    
+      if(charPosition[key].xmin <= xCheck && charPosition[key].xmax >= xCheck && charPosition[key].ymin <= yCheck && charPosition[key].ymax >= yCheck) {
+        console.log(charPosition[key].id);
+        if(checkArr.includes(charPosition[key].id)) return;
+         // let charName = await call(value.id);
+        // console.log(`You found ${charName}!`);
+        checkArr.push(charPosition[key].id)
+        console.log(checkArr);
       } else return;
-    }
-
+    });
+    
     return;
     
   };
